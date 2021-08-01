fba536b50e190ed35e083daad8ab1a7ece8df8ed
martincadek <marti.cadek@gmail.com>
2021-08-01 15:47:10 +0200

    passed checks for custom_snake_case.R

--- R/custom_snake_case.R
+++ R/custom_snake_case.R
@@ -3,6 +3,8 @@
 #' @description This function takes vector of strings and turns each string to snake case format.
 #' The function also replaces any % symbols with "perc".
 #' @param x Vector of strings to coerce to snake case.
+#' @keywords snake
+#' @export
 #' @examples
 #' custom_snake_case("50 % of lions")
 
