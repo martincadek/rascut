e77e301313a69d93d8c05548901aca7fd328c277
martincadek <marti.cadek@gmail.com>
2021-08-01 15:42:17 +0200

    adding function custom_snake_case

--- R/custom_snake_case.R
+++ R/custom_snake_case.R
@@ -1,14 +1,13 @@
 #' @title Coerce strings into snake case formatting
 #'
 #' @description This function takes vector of strings and turns each string to snake case format.
-#' The function also replaces any % symbols with "perc". Further modifiers can be added before
-#' the function internally uses the snakecase::to_any_case(x, case = "snake").
+#' The function also replaces any % symbols with "perc".
 #' @param x Vector of strings to coerce to snake case.
 #' @examples
-#' custom_snake_case(rownames(mtcars))
+#' custom_snake_case("50 % of lions")
 
 custom_snake_case <- function(x){
      x <- gsub("[[:blank:]]", "_", gsub("%", "perc", tolower(trimws(x))))
-     x <- to_any_case(x, case = "snake")
+     x <- snakecase::to_any_case(x, case = "snake")
      return(x)
 }
